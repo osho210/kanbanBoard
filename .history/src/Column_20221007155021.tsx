71e7d530a1f4282b60862a5ca0daab213ec89959
osho210 <anysense.yamatsu@gmail.com>
2022-10-07 16:00:10 +0900

    [Add] InputFormの作成

---
+++ .history/src/Column_20221007155021.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import * as color from './color'
+import { Card } from './Card'
+import { PlusIcon } from './icon'
+import { InputForm as _InputForm } from './InputForm'
+
+export function Column({
+  title,
+  cards,
+}: {
+  title?: string
+  cards: {
+    id: string
+    text?: string
+  }[]
+}) {
+  const totalCount = cards.length
+
+  const [text, setText] = useState('')
+
+  const [inputMode, setInputMode] = useState(false)
+  const toggleInput = () => setInputMode(v => !v)
+  const confirmInput = () => setText('')
+  const cancelInput = () => setInputMode(false)
+
+  return (
+    <Container>
+      <Header>
+        <CountBadge>{totalCount}</CountBadge>
+        <ColumnName>{title}</ColumnName>
+
+        -        <AddButton />
+        <AddButton onClick={toggleInput} />
+      </Header>
+
+      {inputMode && (
+        <InputForm
+          value={text}
+          onChange={setText}
+          onConfirm={confirmInput}
+          onCancel={cancelInput}
+        />
+      )}
+
+      <VerticalScroll>
+        {cards.map(({ id, text }) => (
+          <Card key={id} text={text} />
+        ))}
+      </VerticalScroll>
+    </Container>
+  )
+}
+
+# 中略
+
+const AddButton = styled.button.attrs({
+  type: 'button',
+  children: <PlusIcon />,
+})`
+   margin-left: auto;
+   color: ${color.Black};
+
+   :hover {
+     color: ${color.Blue};
+   }
+ `
+
+const InputForm = styled(_InputForm)`
+  padding: 8px;
+`
+
+const VerticalScroll = styled.div`
+   height: 100%;
+   padding: 8px;
+   overflow-y: auto;
+   flex: 1 1 auto;
+
+   > :not(:first-child) {
+     margin-top: 8px;
+   }
---
+++ .history/src/InputForm_20221005100000.tsx
@@ -0,0 +1,85 @@
+// 受け取った値の表示・ボタンクリックや入力の通知がInputFormの責務
+import React, { useRef, useEffect, useState } from 'react'
+import styled from 'styled-components'
+import * as color from './color'
+import { Button, ConfirmButton } from './Button'
+
+// 第一引数がprops
+export function InputForm({ 
+    value,
+     onChange, 
+     onConfirm,
+      onCancel, 
+      className, }
+    : {
+        // 引数で取得する変数の型宣言(voidは関数の処理のみを実行)
+        // value?: string
+        // onChange?(value: string): void
+        onConfirm?(): void
+        onCancel?(): void
+        className?: string
+    }) {
+    // valueが存在しない場合に処理を実行？
+    const disabled = !value?.trim()
+    // confirm表示処理
+    const handleConfirm = () => {
+        // disabledが存在すると実行しない
+        if (disabled) return
+        onConfirm?.()
+    }
+
+    return (
+        <Container className={className}>
+            <Input
+                autoFocus
+                placeholder="Enter a note"
+                value={value}
+                onChange={ev => onChange?.(ev.currentTarget.value)}
+                onKeyDown={ev => {
+                    if (!((ev.metaKey || ev.ctrlKey) && ev.key === 'Enter')) return
+                    handleConfirm()
+                }}
+            />
+
+            <ButtonRow>
+                <AddButton disabled={disabled} onClick={handleConfirm} />
+                <CancelButton onClick={onCancel} />
+            </ButtonRow>
+        </Container>
+    )
+}
+
+const Container = styled.div``
+
+const Input = styled.textarea`
+  display: block;
+  width: 100%;
+  margin-bottom: 8px;
+  border: solid 1px ${color.Silver};
+  border-radius: 3px;
+  padding: 6px 8px;
+  background-color: ${color.White};
+  font-size: 14px;
+  line-height: 1.7;
+
+  :focus {
+    outline: none;
+    border-color: ${color.Blue};
+  }
+`
+
+const ButtonRow = styled.div`
+  display: flex;
+
+  > :not(:first-child) {
+    margin-left: 8px;
+  }
+`
+
+const AddButton = styled(ConfirmButton).attrs({
+    children: 'Add',
+})``
+
+const CancelButton = styled(Button).attrs({
+    children: 'Cancel',
+})``
